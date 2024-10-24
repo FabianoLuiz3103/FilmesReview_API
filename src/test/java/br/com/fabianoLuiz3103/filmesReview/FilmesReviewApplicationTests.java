a9155155c37bd712409b1e80efa14cce6d9f9486
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2024-10-24 10:01:12 -0300

    iniciando projeto

---
+++ src/test/java/br/com/fabianoLuiz3103/filmesReview/FilmesReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabianoLuiz3103.filmesReview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmesReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
