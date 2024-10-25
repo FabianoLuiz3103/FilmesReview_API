20a98480519b2754a062e6654a78d9377ee83206
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2024-10-25 14:27:15 -0300

    alterando dto filme

--- src/main/java/br/com/fabianoLuiz3103/filmesReview/dto/filme/CreateAndUpdateFilmeDTO.java
+++ src/main/java/br/com/fabianoLuiz3103/filmesReview/dto/filme/CreateAndUpdateFilmeDTO.java
@@ -1,16 +1,14 @@
 package br.com.fabianoLuiz3103.filmesReview.dto.filme;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public record CreateAndUpdateFilmeDTO(
 
         @NotBlank(message = "Título é obrigatório! ")
         String titulo,
         @NotNull(message = "Ano é obrigatório! ")
-        @Size(min = 4, max = 4, message = "Ano deve ter 4 caracteres! ")
+        @Min(value = 1900, message = "Ano deve ser pelo menos 1900")
+        @Max(value = 2100, message = "Ano deve ser no máximo 2100")
         @Positive(message = "Ano deve ser um número positivo")
         Integer ano
 ) {
